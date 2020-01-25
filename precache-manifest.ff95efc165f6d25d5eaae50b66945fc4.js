7ab0fd7dc1b13f1de5c1774127ca26cafa9bb10b
mateusz-fortuna <mat.fortuna@wp.pl>
2020-01-25 22:12:07 +0100

    Updates

---
+++ precache-manifest.ff95efc165f6d25d5eaae50b66945fc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c87241f1496eb7c4c6869e43d52b4f",
+    "url": "/foodbot-website/index.html"
+  },
+  {
+    "revision": "7d9593028a54fb01dafc",
+    "url": "/foodbot-website/static/css/2.d4ac2851.chunk.css"
+  },
+  {
+    "revision": "7eccdd827568ee9d9786",
+    "url": "/foodbot-website/static/css/main.5c5bcae5.chunk.css"
+  },
+  {
+    "revision": "7d9593028a54fb01dafc",
+    "url": "/foodbot-website/static/js/2.c04e0cae.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/foodbot-website/static/js/2.c04e0cae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7eccdd827568ee9d9786",
+    "url": "/foodbot-website/static/js/main.412ea5f4.chunk.js"
+  },
+  {
+    "revision": "6335ddfbeebb8129cfb0",
+    "url": "/foodbot-website/static/js/runtime-main.ac0581cb.js"
+  },
+  {
+    "revision": "f00a6669be183c7fb6d414b6c50cc937",
+    "url": "/foodbot-website/static/media/food-printer-w-background.f00a6669.jpg"
+  }
+]);
