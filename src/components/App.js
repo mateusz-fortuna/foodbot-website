7f87ab6427bac6770ebbc6e9d7c16082193bfc28
mateusz-fortuna <mat.fortuna@wp.pl>
2020-01-25 21:27:26 +0100

    menu works at 100 percent

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,12 @@ import './App.sass';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 
 import { Container, Button } from 'react-bootstrap';
-import Menu from './Menu';
+import { CSSTransition } from 'react-transition-group';
 import Intro from './Intro';
 import Home from './Home';
 import About from './About';
@@ -61,6 +62,9 @@ const App = () => {
       },
       menuLines: {
         backgroundColor: menuLinesBackground
+      },
+      menuLink: {
+        fontSize: ( width >= 1200 ) ? Math.floor( 0.06 * width ) : null
       }
     }
   }
@@ -85,9 +89,177 @@ const App = () => {
           <span className="bottomLine" style={ state.style.menuLines } />
         </Button>
 
+
+        {/* ----------MENU---------- */}
+
+
         {
           menuIsOpen && (
-            <Menu menuButtonIsClicked={ menuButtonIsClicked } />
+            <CSSTransition
+              in={ menuButtonIsClicked }
+              unmountOnExit={ true }
+              timeout={ 1700 }
+            >
+            <div className="menu">
+
+              {/* ----------MENU STRIPS---------- */}
+
+              <CSSTransition
+                in={ menuButtonIsClicked }
+                appear={ true }
+                unmountOnExit={ true }
+                timeout={ {
+                  enter: 600,
+                  appear: 600,
+                  exit: 1100
+                } }
+                classNames="menuStripOne"
+              >
+                <div className="menuStrip menuStripOne" />
+              </CSSTransition>
+              <CSSTransition
+                in={ menuButtonIsClicked }
+                appear={ true }
+                unmountOnExit={ true }
+                timeout={ {
+                  enter: 800,
+                  appear: 800,
+                  exit: 1300
+                } }
+                classNames="menuStripTwo"
+              >
+                <div className="menuStrip menuStripTwo">
+                  <div className="verticalLineMenu" />
+
+                  {/* ----------MENU LINKS---------- */}
+                  
+                  <div className="linksWrapper">
+
+                    <div className="linkWrapper homeWrapper">
+                      <CSSTransition
+                        in={ menuButtonIsClicked }
+                        appear={ true }
+                        unmountOnExit={ true }
+                        timeout={ 500 }
+                        classNames="menuLinkHome"
+                      >
+                        <Link to="/">
+                          <h1
+                            onClick={ handleMenuButtonClick }
+                            className="menuLink menuLinkHome"
+                            style={ state.style.menuLink }
+                          >Home</h1>
+                        </Link>
+                      </CSSTransition>
+                    </div>
+                    
+                    <div className="linkWrapper aboutWrapper">
+                      <CSSTransition
+                        in={ menuButtonIsClicked }
+                        appear={ true }
+                        unmountOnExit={ true }
+                        timeout={ 600 }
+                        classNames="menuLinkAbout"
+                      >
+                        <Link to="/about">
+                          <h1
+                            onClick={ handleMenuButtonClick }
+                            className="menuLink menuLinkAbout"
+                            style={ state.style.menuLink }  
+                          >About</h1>
+                        </Link>
+                      </CSSTransition>  
+                    </div>
+
+                    <div className="linkWrapper galleryWrapper">
+                      <CSSTransition
+                        in={ menuButtonIsClicked }
+                        appear={ true }
+                        unmountOnExit={ true }
+                        timeout={ 700 }
+                        classNames="menuLinkGallery"
+                      >
+                        <Link to="/gallery">
+                          <h1
+                            onClick={ handleMenuButtonClick }
+                            className="menuLink menuLinkGallery"
+                            style={ state.style.menuLink }
+                          >Gallery</h1>
+                        </Link>
+                      </CSSTransition>  
+                    </div>
+
+                    <div className="linkWrapper contactWrapper">
+                      <CSSTransition
+                        in={ menuButtonIsClicked }
+                        appear={ true }
+                        unmountOnExit={ true }
+                        timeout={ 800 }
+                        classNames="menuLinkContact"
+                      >
+                        <Link to="/contact">
+                          <h1
+                            onClick={ handleMenuButtonClick }
+                            className="menuLink menuLinkContact"
+                            style={ state.style.menuLink }
+                          >Contact</h1>
+                        </Link>
+                      </CSSTransition>  
+                    </div>
+                    
+                    <div className="linkWrapper blogWrapper">
+                      <CSSTransition
+                        in={ menuButtonIsClicked }
+                        appear={ true }
+                        unmountOnExit={ true }
+                        timeout={ 900 }
+                        classNames="menuLinkBlog"
+                      >
+                        <Link to="/blog">
+                          <h1
+                            onClick={ handleMenuButtonClick }
+                            className="menuLink menuLinkBlog"
+                            style={ state.style.menuLink }
+                          >Blog</h1>
+                        </Link>
+                      </CSSTransition> 
+                    </div>
+                    
+                  </div>
+
+                </div>
+              </CSSTransition>
+              <CSSTransition
+                in={ menuButtonIsClicked }
+                appear={ true }
+                unmountOnExit={ true }
+                timeout={ {
+                  enter: 1000,
+                  appear: 1000,
+                  exit: 1500
+                } }
+                classNames="menuStripThree"
+              >
+                <div className="menuStrip menuStripThree" />
+              </CSSTransition>
+              <CSSTransition
+                in={ menuButtonIsClicked }
+                appear={ true }
+                unmountOnExit={ true }
+                timeout={ {
+                  enter: 1200,
+                  appear: 1200,
+                  exit: 1700
+                } }
+                classNames="menuStripFour"
+              >
+                <div className="menuStrip menuStripFour">
+                <div className="verticalLineMenu" />
+                </div>
+              </CSSTransition>
+              
+            </div>
+            </CSSTransition>
           )
         }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './index.sass';
