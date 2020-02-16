70567499d5c87a4d92ef7de51255294c281f03f6
mateusz-fortuna <mat.fortuna@wp.pl>
2020-02-16 23:33:59 +0100

    transitions v1 done but learn more doesn't works

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Row } from 'react-bootstrap';
+
+import { throttle } from '../../assets/js/throttle';
+import Transition from '../Transition';
+
 
 const Blog = () => {
+  //----------SCROLL & SWIPE NAVIGATION----------//
+  
+  const [ mountTransition, setMountTransition ] = useState( false );
+
+  let timeoutPrevPage = null;
+  const timeoutPrevPageFn = () => {
+    timeoutPrevPage = setTimeout( () => { window.location.href = '/#/gallery'; }, 1300 );
+  };
+
+  const handleWheel = event => {
+    if ( event.deltaY < 0 ) {
+      setMountTransition( !mountTransition );
+      timeoutPrevPageFn();   
+    }
+  };
+
+  const wheelEvent = event => {
+    throttle( handleWheel( event ), 1300 )
+  };
+
+  let touchStart = 0;
+
+  const handleTouchStart = event => {
+    touchStart = event.touches[ 0 ].pageY;
+  };
+
+  const handleTouchEnd = event => {
+    const touchEnd = event.changedTouches[ 0 ].pageY;
+    
+    if ( touchStart < touchEnd ) {
+      setMountTransition( !mountTransition );
+      timeoutPrevPageFn();
+    }
+  };
+
+  useEffect( () => {
+    window.addEventListener( 'wheel', wheelEvent , false );
+    window.addEventListener( 'touchstart', handleTouchStart, { passive: true } );
+    window.addEventListener( 'touchend', handleTouchEnd, false );
+  
+    return () => {
+      clearTimeout( timeoutPrevPage );
+
+      window.removeEventListener( 'wheel', wheelEvent, false );
+      window.removeEventListener( 'touchstart', handleTouchStart, { passive: true } );
+      window.removeEventListener( 'touchend', handleTouchEnd, false );
+    };
+  } );
+
   return (
-    <div>
+    <Row className="blog">
+      { mountTransition && ( <Transition mountTransition={ mountTransition }/> ) }
       Blog
-    </div>
+    </Row>
   );
 };
 
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Row } from 'react-bootstrap';
+
+import Transition from '../Transition';
+import { throttle } from '../../assets/js/throttle';
 
 const Gallery = () => {
+  //----------SCROLL & SWIPE NAVIGATION----------//
+  
+  const [ mountTransition, setMountTransition ] = useState( false );
+
+
+  let timeoutNextPage = null;
+  let timeoutPrevPage = null;
+  const timeoutNextPageFn = () => {
+    timeoutNextPage = setTimeout( () => { window.location.href = '/#/contact'; }, 1300 );
+  };
+  const timeoutPrevPageFn = () => {
+    timeoutPrevPage = setTimeout( () => { window.location.href = '/#/about'; }, 1300 );
+  };
+
+  const handleWheel = event => {
+    if ( event.deltaY > 0 ) {
+      setMountTransition( !mountTransition );
+      timeoutNextPageFn();   
+    } else {
+      setMountTransition( !mountTransition );
+      timeoutPrevPageFn();
+    }
+  };
+
+  const wheelEvent = event => {
+    throttle( handleWheel( event ), 1300 )
+  };
+
+  let touchStart = 0;
+
+  const handleTouchStart = event => {
+    touchStart = event.touches[ 0 ].pageY;
+  };
+
+  const handleTouchEnd = event => {
+    const touchEnd = event.changedTouches[ 0 ].pageY;
+    
+    if ( touchStart > touchEnd ) {
+      setMountTransition( !mountTransition );
+      timeoutNextPageFn();
+    } else {
+      setMountTransition( !mountTransition );
+      timeoutPrevPageFn();
+    }
+  };
+
+  useEffect( () => {
+    window.addEventListener( 'wheel', wheelEvent , false );
+    window.addEventListener( 'touchstart', handleTouchStart, { passive: true } );
+    window.addEventListener( 'touchend', handleTouchEnd, false );
+  
+    return () => {
+      clearTimeout( timeoutNextPage );
+      clearTimeout( timeoutPrevPage );
+
+      window.removeEventListener( 'wheel', wheelEvent, false );
+      window.removeEventListener( 'touchstart', handleTouchStart, { passive: true } );
+      window.removeEventListener( 'touchend', handleTouchEnd, false );
+    };
+  } );
+
   return (
-    <div>
+    <Row className="gallery">
+      { mountTransition && ( <Transition mountTransition={ mountTransition }/> ) }
       Gallery
-    </div>
+    </Row>
   );
 };
 
