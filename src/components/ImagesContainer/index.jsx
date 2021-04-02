ea11c4b67a10fa663ce19244583a9c1d303ede6d
Mateusz Fortuna <mat.fortuna@wp.pl>
2021-04-02 22:26:25 +0200

    Fix exhaustive-deps in gallery

--- src/components/ImagesContainer/index.jsx
+++ src/components/ImagesContainer/index.jsx
@@ -1,56 +1,57 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import { LanguageContext } from '../../assets/js/context/languageContext';
 import './index.sass';
 
-const ImagesContainer = () => {
+const ImagesContainer = React.forwardRef((props, ref) => {
+  // Get names of all images in gallery
   const languageContext = useContext(LanguageContext);
   const { imagesNames } = languageContext.dictionary.gallery;
 
-  const [images, setImages] = useState([]);
-  const imagesRefs = useRef([]);
-
+  // Set paths to those images
   const imagesPath = require.context('../../assets/images/gallery', true);
   const thumbnailsPath = require.context('../../assets/images/gallery/thumbnails', true);
 
-  const initImages = () => {
-    imagesNames.map((name, index) => {
-      const image = (
-        <div className="imageWrapper" key={`${name}Wrapper`}>
-          <img
-            width={1920}
-            src={thumbnailsPath(`./${name}.jpg`)}
-            data-src={imagesPath(`./${name}.jpg`)}
-            alt={name}
-            key={name}
-            ref={(el) => {
-              imagesRefs.current[index] = el;
-            }}
-            className="galleryImage thumbnail"
-          />
-        </div>
-      );
-      return setImages((imagesArr) => [...imagesArr, image]);
-    });
-  };
+  // Store references to <img> elements
+  const [imgRefs, setImgRefs] = useState([]);
+  const imgRef = useCallback((imgNode) => setImgRefs((oldArray) => [...oldArray, imgNode]), []);
 
-  const loadFullSizePhotos = () => {
-    imagesRefs.current.map((node) => {
-      node.setAttribute('src', node.getAttribute('data-src'));
-      node.removeAttribute('data-src');
-      return node.classList.remove('thumbnail');
-    });
-  };
+  // Generate the images with their wrapper dynamically
+  // Load the thumbnails by default
+  const images = imagesNames.map((name) => (
+    <div className="imageWrapper" key={`${name}Wrapper`}>
+      <img
+        width={1920}
+        src={thumbnailsPath(`./${name}.jpg`)}
+        data-src={imagesPath(`./${name}.jpg`)}
+        alt={name}
+        key={name}
+        ref={imgRef}
+        className="galleryImage thumbnail"
+      />
+    </div>
+  ));
 
   useEffect(() => {
-    initImages();
-    window.addEventListener('load', loadFullSizePhotos);
-
-    return () => {
-      window.removeEventListener('load', loadFullSizePhotos);
+    // When the full resolution photos are loaded, replace them with the thumbnails
+    const setFullSizePhotos = () => {
+      imgRefs.forEach((node) => {
+        node.setAttribute('src', node.getAttribute('data-src'));
+        node.removeAttribute('data-src');
+        node.classList.remove('thumbnail');
+      });
     };
-  }, []);
 
-  return <div className="imagesContainer">{images}</div>;
-};
+    window.addEventListener('load', setFullSizePhotos);
+    return () => {
+      window.removeEventListener('load', setFullSizePhotos);
+    };
+  });
+
+  return (
+    <div className="imagesContainer" ref={ref}>
+      {images}
+    </div>
+  );
+});
 
 export default ImagesContainer;
