ab0a543576539ef60e7dfaa70b2bacbcc8a8bbba
Mateusz Fortuna <mat.fortuna@wp.pl>
2021-05-09 12:50:35 +0200

    Move the business logic from the Contact to Form

--- client/src/components/Contact/Form/index.js
+++ client/src/components/Contact/Form/index.js
@@ -1,46 +1,133 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
+import axios from 'axios';
+import { debounce } from 'lodash';
 import './index.sass';
 
-const ContactForm = ({ formRef, inputRef, submitButtonRef, inputChangeHandler }) => (
-  <form className="contact__form" ref={formRef}>
-    <div className="contact__form_group form-group">
-      <label htmlFor="inputName">Name</label>
-      <input
-        type="text"
-        name="name"
-        required
-        className="form-control"
-        placeholder="Enter your name"
-        ref={inputRef}
-        onChange={inputChangeHandler}
-      />
-    </div>
-    <div className="contact__form_group form-group">
-      <label htmlFor="inputEmail">Email address</label>
-      <input
-        type="email"
-        name="email"
-        required
-        className="form-control"
-        placeholder="Enter your email"
-        ref={inputRef}
-        onChange={inputChangeHandler}
-      />
-    </div>
-    <div className="contact__form_group form-group">
-      <label htmlFor="inputMessage">Message</label>
-      <textarea
-        name="feedback"
-        required
-        rows="7"
-        className="form-control"
-        placeholder="What would you like to chat about?"
-        ref={inputRef}
-        onChange={inputChangeHandler}
-      />
-    </div>
-    <input type="submit" value="Submit" ref={submitButtonRef} />
-  </form>
-);
+const ContactForm = ({
+  setRenderCursor,
+  setCursorColor,
+  setPreventNavigation,
+  submitButtonRef,
+}) => {
+  // ----------STATE---------- //
+
+  const [formData, setFormData] = useState({});
+  const [inputs, setInputs] = useState([]);
+  const [form, setForm] = useState(null);
+
+  // ----------REFERENCES---------- //
+
+  const inputRef = useCallback((node) => setInputs((inputs) => [...inputs, node]), []);
+  const formRef = useCallback((node) => setForm(node), []);
+
+  // ----------HELPERS---------- //
+
+  const preventNavigation = (inputValue) => {
+    // Prevent scroll navigation when the input contains a text
+    if (inputValue) return setPreventNavigation(true);
+    return setPreventNavigation(false);
+  };
+
+  // ----------HANDLERS---------- //
+
+  const showCursor = () => setRenderCursor(true);
+  const hideCursor = () => setRenderCursor(false);
+  const setDarkCursor = () => setCursorColor('dark');
+  const setLightCursor = () => setCursorColor('light');
+
+  const handleInputChange = (event) => {
+    const { target } = event;
+
+    event.preventDefault();
+    preventNavigation(target.value);
+
+    const name = target.name;
+    const value = target.value;
+    setFormData((formData) => ({ ...formData, [name]: value }));
+
+    return debounce(handleInputChange, 300);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    return axios
+      .post('http://localhost:3001/contact/submit', formData)
+      .catch((err) => console.error(err));
+  };
+
+  // ----------EVENT LISTENERS---------- //
+
+  useEffect(() => {
+    if (form) {
+      form.addEventListener('submit', handleFormSubmit);
+      form.addEventListener('mouseenter', setDarkCursor);
+      form.addEventListener('mouseleave', setLightCursor);
+    }
+    if (inputs.length) {
+      inputs.forEach((input) => {
+        input.addEventListener('focusin', hideCursor);
+        ['focusout', 'mouseleave'].forEach((evt) => input.addEventListener(evt, showCursor));
+      });
+    }
+
+    return () => {
+      if (form) {
+        form.removeEventListener('submit', handleFormSubmit);
+        form.removeEventListener('mouseenter', setDarkCursor);
+        form.removeEventListener('mouseleave', setLightCursor);
+      }
+      if (inputs.length) {
+        inputs.forEach((input) => {
+          input.removeEventListener('focusin', hideCursor);
+          ['focusout', 'mouseleave'].forEach((evt) => input.removeEventListener(evt, showCursor));
+        });
+      }
+    };
+  });
+
+  // ----------JSX---------- //
+
+  return (
+    <form className="contact__form" ref={formRef}>
+      <div className="contact__form_group form-group">
+        <label htmlFor="inputName">Name</label>
+        <input
+          type="text"
+          name="name"
+          required
+          className="form-control"
+          placeholder="Enter your name"
+          ref={inputRef}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="contact__form_group form-group">
+        <label htmlFor="inputEmail">Email address</label>
+        <input
+          type="email"
+          name="email"
+          required
+          className="form-control"
+          placeholder="Enter your email"
+          ref={inputRef}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="contact__form_group form-group">
+        <label htmlFor="inputMessage">Message</label>
+        <textarea
+          name="feedback"
+          required
+          rows="7"
+          className="form-control"
+          placeholder="What would you like to chat about?"
+          ref={inputRef}
+          onChange={handleInputChange}
+        />
+      </div>
+      <input type="submit" value="Submit" ref={submitButtonRef} />
+    </form>
+  );
+};
 
 export default ContactForm;
--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -1,87 +1,23 @@
-import React, { useCallback, useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useCallback, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { debounce } from 'lodash';
-import Cursor from '../Cursor';
 import TransitionOut from '../Transitions/TransitionOut';
-import './index.sass';
+import Cursor from '../Cursor';
 import ContactForm from './Form';
+import './index.sass';
 
 const Contact = ({ reference, setPreventNavigation }) => {
   // ----------STATE---------- //
 
-  const [formData, setFormData] = useState({});
-
-  // ----------HANDLERS---------- //
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    axios.post('http://localhost:3001/contact/submit', formData).catch((err) => console.error(err));
-  };
-
-  const handleInputChange = (event) => {
-    event.preventDefault();
-
-    const { target } = event;
-    const name = target.name;
-    const value = target.value;
-
-    setFormData((formData) => ({ ...formData, [name]: value }));
-
-    // Prevent scroll navigation when the input contains a text
-    if (target.value) return setPreventNavigation(true);
-    return setPreventNavigation(false);
-  };
-  const debouncedInputChange = debounce(handleInputChange, 300);
+  const [renderCursor, setRenderCursor] = useState(true);
+  const [cursorColor, setCursorColor] = useState('light');
+  const [submitButton, setSubmitButton] = useState(null);
 
   // ----------REFERENCES---------- //
 
-  const [inputs, setInputs] = useState([]);
-  const [form, setForm] = useState(null);
-  const [submitButton, setSubmitButton] = useState(null);
+  const submitButtonRef = useCallback((node) => setSubmitButton(node), []);
   const cursorReferences = reference.concat({ current: submitButton });
 
-  const inputRef = useCallback((node) => setInputs((inputs) => [...inputs, node]), []);
-  const formRef = useCallback((node) => setForm(node), []);
-  const submitButtonRef = useCallback((node) => setSubmitButton(node), []);
-
-  // ----------THE CURSOR BEHAVIOR ON FORM---------- //
-
-  const [renderCursor, setRenderCursor] = useState(true);
-  const [cursorColor, setCursorColor] = useState('light');
-
-  const showCursor = () => setRenderCursor(true);
-  const hideCursor = () => setRenderCursor(false);
-  const setDarkCursor = () => setCursorColor('dark');
-  const setLightCursor = () => setCursorColor('light');
-
-  // ----------EVENT LISTENERS---------- //
-
-  useEffect(() => {
-    if (form) {
-      form.addEventListener('submit', handleFormSubmit);
-      form.addEventListener('mouseenter', setDarkCursor);
-      form.addEventListener('mouseleave', setLightCursor);
-    }
-    inputs.length &&
-      inputs.forEach((input) => {
-        input.addEventListener('focusin', hideCursor);
-        ['focusout', 'mouseleave'].forEach((evt) => input.addEventListener(evt, showCursor));
-      });
-
-    return () => {
-      if (form) {
-        form.removeEventListener('submit', handleFormSubmit);
-        form.removeEventListener('mouseenter', setDarkCursor);
-        form.removeEventListener('mouseleave', setLightCursor);
-      }
-      inputs.length &&
-        inputs.forEach((input) => {
-          input.removeEventListener('focusin', hideCursor);
-          ['focusout', 'mouseleave'].forEach((evt) => input.removeEventListener(evt, showCursor));
-        });
-    };
-  });
+  // ----------JSX---------- //
 
   return (
     <Row className="contact align-items-center">
@@ -92,10 +28,10 @@ const Contact = ({ reference, setPreventNavigation }) => {
       <Col md={6}>
         <h1 className="contact__description">Do you have any questions? Contact us!</h1>
         <ContactForm
-          formRef={formRef}
-          inputRef={inputRef}
+          setRenderCursor={setRenderCursor}
+          setCursorColor={setCursorColor}
+          setPreventNavigation={setPreventNavigation}
           submitButtonRef={submitButtonRef}
-          inputChangeHandler={debouncedInputChange}
         />
       </Col>
     </Row>
